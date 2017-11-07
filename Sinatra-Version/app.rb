eb5363453878e97fa8353ceb1f006a0ae555683c
Joshua Prpich <joshuaprpich@gmail.com>
2017-11-07 11:53:17 -0500

    add answers functionality

--- Sinatra-Version/app.rb
+++ Sinatra-Version/app.rb
@@ -4,6 +4,11 @@ questions = [
 "How old is Ozu?"
 ]
 
+answers = [
+"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+]
+
+
 
 get '/' do
   erb :index, locals: {questions: questions}
@@ -26,11 +31,16 @@ get '/tecnologia' do
 end
 
 get '/answers' do
-  erb :answers
+  erb :answers, locals: {answers: answers}
 end
 
 
 post '/questions' do
   questions << params[:question]
   redirect to('/')
+end
+
+post '/answers' do
+  answers << params[:answer]
+  redirect to('/answers')
 end
