191dce2574912a4e26e46bb9a8e30d19aade9f38
Fermedina <fermed28@gmai.com>
2017-11-14 18:13:55 -0500

    run app even if song list is empty

--- config/database.rb
+++ config/database.rb
@@ -36,7 +36,7 @@ ActiveRecord::Base.establish_connection(
   :host     => db.host,
   :port     => db.port,
   :username => db.user,
-  :password => db.password,
+  :password => '123',
   :database => DB_NAME,
   :encoding => 'utf8'
 )
