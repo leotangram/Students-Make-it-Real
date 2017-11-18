6e19a905671dc64bb0e944a97f37baac57a4c809
Joshua Prpich <joshuaprpich@gmail.com>
2017-11-18 16:37:19 -0500

    add js practice

--- public/js/application.js
+++ public/js/application.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
-  alert("Welcome To JS Territory")
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  var img = document.querySelector("img") 
+  img.addEventListener("click", function(){
+    this.style.width = "300px"
+  })
+
+  var img2 = document.querySelectorAll("img")[1]
+  img2.addEventListener("click", function(){
+    this.classList.toggle("kitty")
+  })
+  
 });
